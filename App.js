af3c512075df10e7f6fba5435638e333136a3be5
Liudas <repkovas@gmail.com>
2022-03-07 18:07:24 +0200

    functionally done, ugly to.

--- App.js
+++ App.js
@@ -1,31 +1,69 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import Wheather from './components/WheatherComponent';
-import Loading from './components/LoadingComponent';
-import * as Location from 'expo-location';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, BackHandler } from "react-native";
+import Wheather from "./components/WheatherComponent";
+import Loading from "./components/LoadingComponent";
+import * as Location from "expo-location";
+import { useEffect, useState } from "react";
 
 export default function App() {
+  // sukuriam steitus
+  const [isLoading, setIsLoading] = useState(true);
+  const [location, setLocation] = useState(null);
+  const [wheather, setWheather] = useState(null);
+
+  //
+  useEffect(async () => {
+    const locationInfo = await getLocationPermissionsAndUserLocation();
+    const wheatherData = await getWheatherData(locationInfo.latitude, locationInfo.longitude);
+    setIsLoading(false);
+    setLocation(locationInfo);
+    setWheather(wheatherData);
+  }, []);
+
+  const ShowWheatherIfNotLoading = () => {
+    if (isLoading) {
+      return <Loading></Loading>;
+    } else {
+      return <Wheather location={location} wheather={wheather}></Wheather>;
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <ShowWheatherIfNotLoading isLoading={true}></ShowWheatherIfNotLoading>
+      <ShowWheatherIfNotLoading
+        isLoading={isLoading}
+      ></ShowWheatherIfNotLoading>
       <StatusBar style="auto" />
     </View>
   );
 }
 
-function ShowWheatherIfNotLoading(props) {
-  if(props.isLoading) {
-    return (<Loading></Loading>)
+
+async function getLocationPermissionsAndUserLocation() {
+  // klausiam userio permisionu
+  let permissionsResult = await Location.requestForegroundPermissionsAsync();
+  if (permissionsResult.status == "granted") {
+    // gaunam location duomenis
+    let location = await Location.getCurrentPositionAsync({});
+
+    return location.coords;
   } else {
-    return (<Wheather></Wheather>)
+    // metam errora jei useris neduoda permissionu
+    alert("Could not get permissions. Allow to access location to use app.");
   }
+}
+
+async function getWheatherData(lat, lon) {
+  const requestResult = await fetch('https://api.openweathermap.org/data/2.5/weather?units=metric&lat='+lat+'&lon='+lon+'&appid=5cf2bd2ce0a3b3cf847ef5502316b2ac');
+  const resultJSON = await requestResult.json();
+  return resultJSON;
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
--- components/WheatherComponent.js
+++ components/WheatherComponent.js
@@ -1,9 +1,30 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from "react-native";
 
-export default function Wheather() {
+export default function Wheather(props) {
+  console.log(props)
+
+  const ShowLocationAndWheatherIfItExists = () => {
+    if (props.location === null || props.wheather === null) {
+      return <Text>No info.</Text>;
+    } else {
+      return (
+        <View>
+          <Text>Location:</Text>
+          <Text>{props.location.latitude}</Text>
+          <Text>{props.location.longitude}</Text>
+
+          
+          <Text>Temperature:</Text>
+          <Text>{props.wheather.main.temp}</Text>
+        </View>
+      );
+    }
+  };
+
   return (
     <View style={styles.container}>
       <Text>Wheather</Text>
+      {ShowLocationAndWheatherIfItExists()}
     </View>
   );
 }
@@ -11,8 +32,8 @@ export default function Wheather() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "red",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
